3ce3d3b1604d66abbf73dba743fa6c4d1e80ea9e
Rupin Chheda <rupin.chheda@gmail.com>
2025-05-27 18:09:49 +0530

    Add files via upload

---
+++ BasicRecieve.py
@@ -0,0 +1,16 @@
+from machine import Pin, UART
+
+import time
+
+# Initialize UART2 and Direction Control pin
+SerialPort = UART(2, baudrate=9600, tx=17, rx=16)  # Adjust TX/RX pins as needed
+control_pin = Pin(26, Pin.OUT)
+
+control_pin.value(0)  # Set direction to receive
+
+while (True):
+    #print("Checking")
+    if SerialPort.any():
+        data=SerialPort.read()
+        print(data)
+    
